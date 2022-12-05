3d9bd7918e23c1a0d2006b6cc28e69ddc3e35ce0
jhp2100 <62491987+jhp2100@users.noreply.github.com>
2022-12-05 14:41:04 +0800

    Initial commit

---
+++ diy-part1.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 注释掉lienol大diy1源
+#sed -i 's/^\(.*diy1\)/#&/' feeds.conf.default
+
+# 添加xiaorouji大passwall源
+#sed -i '$a src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
+#sed -i '$a src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;luci' feeds.conf.default
+
+# 注释掉默认的packages源
+#sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
+
+# 添加修改的packages源
+#sed -i '$a src-git packages https://github.com/lxhao61/openwrt-packages-1.git;21.02' feeds.conf.default
